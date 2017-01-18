e13a434cf9511c48bd4b55fe7105cdab650d54ce
vc7 <vince78718@gmail.com>
2017-01-18 16:52:59 +0900

    把各自 view controller instance 的 views 放定位

--- viewControllerContainerSample/ViewController.swift
+++ viewControllerContainerSample/ViewController.swift
@@ -8,14 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ContentAViewControllerDelegate, ContentBViewControllerDelegate {
 
     @IBOutlet weak var topViewContainer: UIView!
     @IBOutlet weak var bottomViewContainer: UIView!
     
+    fileprivate var contentAViewController: ContentAViewController = ContentAViewController()
+    fileprivate var contentBViewController: ContentBViewController = ContentBViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.contentAViewController.delegate = self
+        self.contentBViewController.delegate = self
+        
+        // 抽出各自的 main view
+        let contentAView = self.contentAViewController.view!
+        let contentBView = self.contentBViewController.view!
+        
+        // 放入 top view container - A
+        self.contentAViewController.willMove(toParentViewController: self)
+        self.topViewContainer.addSubview(contentAView)
+        // Layout, 加入 constraints - A
+        contentAView.translatesAutoresizingMaskIntoConstraints = false
+        self.topViewContainer.addConstraints([
+            NSLayoutConstraint(item: contentAView, attribute: .top, relatedBy: .equal, toItem: self.topViewContainer, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentAView, attribute: .bottom, relatedBy: .equal, toItem: self.topViewContainer, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentAView, attribute: .leading, relatedBy: .equal, toItem: self.topViewContainer, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentAView, attribute: .trailing, relatedBy: .equal, toItem: self.topViewContainer, attribute: .trailing, multiplier: 1, constant: 0),
+            ])
+        
+        // 放入 bottom view container - B
+        self.contentBViewController.willMove(toParentViewController: self)
+        self.bottomViewContainer.addSubview(contentBView)
+        // Layout, 加入 constraints - B
+        contentBView.translatesAutoresizingMaskIntoConstraints = false
+        self.bottomViewContainer.addConstraints([
+            NSLayoutConstraint(item: contentBView, attribute: .top, relatedBy: .equal, toItem: self.bottomViewContainer, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentBView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomViewContainer, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentBView, attribute: .leading, relatedBy: .equal, toItem: self.bottomViewContainer, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentBView, attribute: .trailing, relatedBy: .equal, toItem: self.bottomViewContainer, attribute: .trailing, multiplier: 1, constant: 0),
+            ])
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +56,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - ContentAViewControllerDelegate
+    
+    func contentAViewController(viewController: ContentAViewController, didSelectAtRow row: Int) {
+        debugPrint("Selecting A at row - \(row)")
+    }
+    
+    // MARK: - ContentBViewControllerDelegate
+    
+    func contentBViewController(viewController: ContentBViewController, didSelectAtRow row: Int) {
+        debugPrint("Selecting B at row - \(row)")
+    }
 
 }
 
